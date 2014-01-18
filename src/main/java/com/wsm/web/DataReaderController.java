bfc2f8ad513e00370fd3dacc9a626fe0e89f928f
mehulgayate <mehulgayate21@gmail.com>
2014-01-18 13:28:53 +0530

    Upload XML File

--- src/main/java/com/wsm/web/DataReaderController.java
+++ src/main/java/com/wsm/web/DataReaderController.java
@@ -1,6 +1,7 @@
 package com.wsm.web;
 
 import java.io.File;
+import java.io.FileOutputStream;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletContext;
@@ -11,15 +12,13 @@ import net.sf.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 import com.evalua.entity.support.DataStoreManager;
-import com.wsm.entity.Report;
+import com.wsm.form.FileUploadForm;
 import com.wsm.processor.ClusterCreator;
 import com.wsm.processor.PMFCalculator;
 import com.wsm.util.XMLParser;
@@ -51,6 +50,27 @@ public class DataReaderController {
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(fXmlFile);
+			JSONObject jsonObject=new JSONObject();
+			jsonObject.put(doc.getDocumentElement().getNodeName(), xmlParser.parseXML(doc));
+			pmfCalculator.JsontoReport(jsonObject);
+			
+			clusterCreator.crateClusters();
+			clusterCreator.allocateCluster();
+			mv.addObject("json",jsonObject);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+		return mv;
+	}
+	
+	@RequestMapping("/upload-xml")
+	public ModelAndView uploadFile(@ModelAttribute(FileUploadForm.key) FileUploadForm fileUploadForm)throws Exception{
+		ModelAndView mv=new ModelAndView("json-string");
+		try {
+			
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fileUploadForm.getXmlFile().getInputStream());
 			JSONObject jsonObject=new JSONObject();
 			jsonObject.put(doc.getDocumentElement().getNodeName(), xmlParser.parseXML(doc));
 			pmfCalculator.JsontoReport(jsonObject);
